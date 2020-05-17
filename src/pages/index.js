bfa612c05bd5970624fd713f0693c835a06129c5
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-05-17 14:37:30 +0100

    basic landing page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,28 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi, I'm Liz!</h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <h2>
+      I’m a freshly-minted software developer from the School of Code’s third
+      cohort.
+    </h2>
+    <p>
+      I studied history and archeology, which was about as useful as you'd
+      imagine in my office jobs after that! I’m drawn to technology and
+      development because I’ve always loved learning, challenges, and the
+      occasional leap into the unknown – not to mention that coding is the
+      closest thing to magic in real life!
+    </p>
+    <p>
+      In 2019, I decided to follow my heart towards technology by learning to
+      code, and I applied to the School of Code. By May 2020, my time as a
+      bootcamper equipped me with the skills I needed for full-stack agile
+      development and completely changed my life.
+    </p>
+
     {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
