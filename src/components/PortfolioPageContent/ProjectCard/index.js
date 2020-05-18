a2dd1ff0183d2d13a40a9b3cecaddfc71ba8b2ef
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-05-19 00:53:44 +0100

    collapse w/ highlighted projects in place; can add more projects easily in the projects.js file

--- src/components/PortfolioPageContent/ProjectCard/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-import css from "./projectCard.module.css"
-
-function ProjectCard() {
-  return <section className={css.projectCard}></section>
-}
-
-export default ProjectCard
--- src/components/PortfolioPageContent/index.js
+++ src/components/PortfolioPageContent/index.js
@@ -1,46 +1,69 @@
 import React from "react"
+import { Collapse } from "antd"
 
-import ProjectCard from "./ProjectCard"
+import css from "./portfolioPageContent.module.css"
+import projects from "./projects"
 
-const projects = [
-  {
-    name: "Volt",
-    date: "13 April-8 May 2020",
-    mainTools: [
-      "React",
-      "NextJS",
-      "Jest",
-      "Serverless",
-      "AWS DynamoDB",
-      "Firebase",
-    ],
-    summary:
-      "For our School of Code final project, we were tasked with creating an app that helps match volunteers with organisations and opportunities. As a team of three, we created Volt, a progressive web app that uses dating app functionality to match volunteers and organisations for a long-term volunteering relationship. I made the personality quiz and matching interface, ensured the app was responsive for both mobile and computer users, and designed the fully RESTful API on the back end.",
-    img: "https://i.ibb.co/TrS0PgC/volt-screenshot.png",
-    repoLink: "",
-  },
-  {
-    name: "West Midlands Virtual Forest",
-    date: "9-13 March 2020",
-    mainTools: ["React", "PostgreSQL"],
-    summary:
-      "For our School of Code mid-term project, our team of four received a brief from the West Midlands Combined Authority to create an interactive page for their Virtual Forest tree-planting initiative. In one week (with three days of actual coding), we built a single-page application that shows trees planted on a map of the WCMA key areas and accepts new trees planted and requests for trees. I designed functional components and logic, linked front end components to the PostgreSQL database tables on the back end, and used CSS to make web, tablet, and mobile versions.",
-    img: "",
-    repoLink:
-      "https://github.com/lizkaufman/front-end-week-8-project-team-indecision",
-  },
-  {
-    name: "",
-    date: "",
-    mainTools: [],
-    summary: "",
-    img: "",
-    repoLink: "",
-  },
-]
+const { Panel } = Collapse
 
 function PortfolioPageContent() {
-  return <div></div>
+  return (
+    <div className={css.portfolioPageContent}>
+      <section id={css.portfolioIntro}>
+        <h1 style={{ fontFamily: "Roboto" }}>My Skills and Portfolio</h1>
+        <p>Placeholder text - intro p describing SoC process</p>
+        <h3 style={{ fontFamily: "Roboto" }}>Technical skills I developed:</h3>
+        <ul>
+          <li></li>
+        </ul>
+      </section>
+      <Collapse
+        accordion
+        style={{
+          background: "#61856e65",
+          fontWeight: "bold",
+          borderRadius: "15px",
+        }}
+      >
+        {projects.map(project => (
+          <Panel header={project.name} key={project.name}>
+            <div className={css.panelContents}>
+              <img
+                className={css.projectImage}
+                alt={project.name}
+                src={project.img}
+              />
+              <div className={css.projectInfo}>
+                <p className={css.projectDate}>{project.date}</p>
+
+                {project.hostedLink && (
+                  <p>
+                    <a
+                      href={project.hostedLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {project.hostedLink}
+                    </a>
+                  </p>
+                )}
+                <p>
+                  <a
+                    href={project.repoLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    GitHub repository
+                  </a>
+                </p>
+                <p className={css.projectSummary}>{project.summary}</p>
+              </div>
+            </div>
+          </Panel>
+        ))}
+      </Collapse>
+    </div>
+  )
 }
 
 export default PortfolioPageContent
