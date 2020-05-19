325aba3711bc0832296323f9e511e3d5bb84c521
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-05-19 02:12:21 +0100

    first iteration of the portfolio page done

--- src/components/PortfolioPageContent/index.js
+++ src/components/PortfolioPageContent/index.js
@@ -6,16 +6,58 @@ import projects from "./projects"
 
 const { Panel } = Collapse
 
+//TODO: Need to add media queries to the CSS to make mobile version of this page!
+
 function PortfolioPageContent() {
   return (
     <div className={css.portfolioPageContent}>
       <section id={css.portfolioIntro}>
         <h1 style={{ fontFamily: "Roboto" }}>My Skills and Portfolio</h1>
-        <p>Placeholder text - intro p describing SoC process</p>
-        <h3 style={{ fontFamily: "Roboto" }}>Technical skills I developed:</h3>
-        <ul>
-          <li></li>
-        </ul>
+        <p>
+          Over the intensive 16 weeks of the School of Code bootcamp, I learned
+          the tools of full-stack development, studied UI/UX design, navigated
+          the intricacies of testing and deployment, and lived and breathed
+          agile methodology.
+        </p>
+        <p>
+          Halfway through the course, we got our first taste of a longer
+          project, a week-long agile sprint to create a website for the West
+          Midlands Combined Authority's Virtual Forest program. We presented
+          what we'd made to a panel of judges (both WMCA employees and local
+          tech industry professionals) at the end.
+        </p>
+        <div id={css.midtermPics}>
+          <img
+            className={css.midtermPic}
+            alt="team for midterm project - planning"
+            src="https://i.ibb.co/3sTxGHx/tree-project2.jpg"
+          />
+          <img
+            className={css.midtermPic}
+            alt="team for midterm project - coding"
+            src="https://i.ibb.co/ZKCqYy7/tree-project.png"
+          />
+          <img
+            className={css.midtermPic}
+            alt="team for midterm project - coding"
+            src="https://i.ibb.co/ZfSKwwt/midterm-presenting-pic.jpg"
+          />
+        </div>
+
+        <p>
+          For the final month, I worked in a team of three to develop Volt, a
+          full stack progressive web app bringing fun dating app functionality
+          to connecting volunteers with organisations and opportunities. Due to
+          global circumstances, the last eight weeks of the course, including
+          this final project, were completely online, which gave us valuable
+          experience communicating and collaborating remotely. We presented this
+          to over 200 attendees at the virtual Demo Day on 14 May 2020.
+        </p>
+        <p>
+          And now that my time as a School of Code bootcamper is over, I'm still
+          coding up a storm, so watch this space for new additions!
+        </p>
+        <h4>Some of my favorite projects so far:</h4>
       </section>
       <Collapse
         accordion
@@ -23,6 +65,8 @@ function PortfolioPageContent() {
           background: "#61856e65",
           fontWeight: "bold",
           borderRadius: "15px",
+          marginTop: "2rem",
+          marginBottom: "2rem",
         }}
       >
         {projects.map(project => (
@@ -62,6 +106,13 @@ function PortfolioPageContent() {
           </Panel>
         ))}
       </Collapse>
+      <p id={css.githubBlurb}>
+        You can view these projects and more of my work at my{" "}
+        <a id={css.githubLink} href="https://github.com/lizkaufman">
+          Github profile
+        </a>
+        .
+      </p>
     </div>
   )
 }
--- src/components/PortfolioPageContent/projects.js
+++ src/components/PortfolioPageContent/projects.js
@@ -2,7 +2,6 @@ const projects = [
   {
     name: "Volt",
     date: "13 April-8 May 2020",
-    mainTools: ["React", "NextJS", "Serverless", "AWS", "Firebase"],
     summary:
       "For our School of Code final project, we were tasked with creating an app that helps match volunteers with organisations and opportunities. As a team of three, we created Volt, a progressive web app that uses dating app functionality to match volunteers and organisations for a long-term volunteering relationship. I made the personality quiz and matching interface, ensured the app was responsive for both mobile and computer users, and designed the fully RESTful API on the back end.",
     img: "https://i.ibb.co/TrS0PgC/volt-screenshot.png",
@@ -12,7 +11,6 @@ const projects = [
   {
     name: "West Midlands Virtual Forest",
     date: "9-13 March 2020",
-    mainTools: ["React", "PostgreSQL"],
     summary:
       "For our School of Code mid-term project, our team of four received a brief from the West Midlands Combined Authority to create an interactive page for their tree-planting initiative. In one week, we built a single-page application that shows trees planted on a map of the WCMA key areas and records new trees planted and requests for trees. I designed functional components/logic, linked the front end to PostgreSQL database tables on the back end, and ensured the page was responsive on web, tablet, and mobile.",
     img: "https://i.ibb.co/XL5DjCj/needatree-screenshot.png",
@@ -23,7 +21,6 @@ const projects = [
   {
     name: "HangCode",
     date: "24-26 March 2020",
-    mainTools: ["React", "Websocket (TBC!)"],
     summary:
       "This project began as a pair programming project during week 10 of School of Code: translate a well-known game into a React app. We chose hangman with a code-themed twist. At the end of the week, we had a functioning one-player game, with a bank of ten React-themed words, one of which is chosen randomly and guessed by the player. I enjoyed working on this so much that I've made my own repo of it to keep tinkering with it further. I'm planning, as a personal project, to make it two-player with socket.io.",
     img: "https://i.ibb.co/wMhPWXg/hangcode-screenshot.png",
@@ -33,7 +30,6 @@ const projects = [
   {
     name: "Quarantine Calculator",
     date: "3 April 2020",
-    mainTools: ["React", "GatsbyJS"],
     summary:
       "At the end of the day that we were first introduced to GatsbyJS, we had a one-hour hackathon to try it out and make something. This was of course during the global circumstances that led to a toilet paper calculator going viral, so our group decided to make something similar, but for beer. It might not be quite as critical as loo roll, but it's good for morale!",
     img: "https://i.ibb.co/r3VY8R0/beer-calc-screenshot.png",
